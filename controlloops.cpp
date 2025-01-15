312f725af87392a744f4ab07883bbfad33806eaf
ansariwaqqas <ansarivakkas28@gmail.com>
2025-01-15 17:59:09 +0530

    controlloop is updated

--- controlloops.cpp
+++ controlloops.cpp
@@ -1,7 +1,33 @@
+/* there are three control loop structure
+    for, while, dowhile, programs */
+
 #include<iostream>
 using namespace std;
 int main() 
 {
-    
+    int i,n;
+    cout<<"Enter the number of terms: ";
+    cin>>n;
+    cout<<"Fibonacci Series: ";
+    for(i=0;i<n;i++)
+    {
+        if(i==0)
+        cout<<0;
+        else if(i==1)
+        cout<<1;
+        else
+        {
+            int a=0,b=1;
+            cout<<a<<" "<<b<<" ";
+            for(int j=2;j<i;j++)
+            {
+                int temp=a+b;
+                a=b;
+                b=temp;
+            }
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
